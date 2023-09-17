cd959be233dfb0893516a9558196939042a4004b
jlbuild <juliabuildbot@gmail.com>
2023-09-17 20:03:18 +0000

    aws_c_http_jll build 0.7.12+0

---
+++ src/aws_c_http_jll.jl
@@ -0,0 +1,9 @@
+# Use baremodule to shave off a few KB from the serialized `.ji` file
+baremodule aws_c_http_jll
+using Base
+using Base: UUID
+import JLLWrappers
+
+JLLWrappers.@generate_main_file_header("aws_c_http")
+JLLWrappers.@generate_main_file("aws_c_http", UUID("3254fc65-9028-534d-aa9d-d76d128babc6"))
+end  # module aws_c_http_jll
