7c2e7f40521306f03efc9e111919be29ce7a36a7
jlbuild <juliabuildbot@gmail.com>
2026-01-06 23:44:19 +0000

    aws_c_http_jll build 0.10.8+0

--- src/aws_c_http_jll.jl
+++ src/aws_c_http_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("aws_c_http")
-JLLWrappers.@generate_main_file("aws_c_http", UUID("3254fc65-9028-534d-aa9d-d76d128babc6"))
+JLLWrappers.@generate_main_file("aws_c_http", Base.UUID("3254fc65-9028-534d-aa9d-d76d128babc6"))
 end  # module aws_c_http_jll
